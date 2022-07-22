4a23f9cc98ef82d09970d6a7e470183563d22137
Matt Koscica <matt.koscica@gmail.com>
2022-07-23 01:50:23 +0200

    new file:   rotatingAltSquares/Square.java

---
+++ rotatingAltSquares/Square.java
@@ -0,0 +1,17 @@
+// import processing.core.PVector;
+// import processing.core.PApplet;
+// 
+// public class Square {
+//   PVector location = new PVector(0,0);
+//   PVector delta = new PVector(0,0);
+//   PApplet processing = new PApplet();
+// 
+//   Square(int x, int y) {
+//     location = new PVector(x, y);
+//   }
+// 
+//   void update() {
+//     this.location.add(this.delta);
+//   }
+// }
+// 
